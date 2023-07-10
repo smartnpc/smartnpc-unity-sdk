8274930d91933be50050719490d2049c48138f19
Royi Bernthal <royibernthal@gmail.com>
2023-07-10 17:32:46 +0800

    gender and language enums

---
+++ Scripts/Runtime/Character/SmartNPCGender.cs
@@ -0,0 +1,8 @@
+namespace SmartNPC
+{
+    public static class SmartNPCGender
+    {
+        public static readonly string Male = "male";
+        public static readonly string Female = "female";
+    }
+}
